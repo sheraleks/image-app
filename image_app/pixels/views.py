864eb59876c76d4b4ad59b8e4d16906a7b4a2d5e
Aleksandr Shcherbakov <sheraleks@outlook.com>
2021-09-15 15:29:14 +0700

    black_white view

--- image_app/pixels/views.py
+++ image_app/pixels/views.py
@@ -3,21 +3,36 @@ import os
 from aiohttp import MultipartReader
 from aiohttp.web import View, json_response, HTTPBadRequest
 
-from image_app.web.utils import download_file
+from image_app.web.utils import download_file, load_image_np, count_pixels
 
 
 class BlackWhiteView(View):
+    # TODO: поле image обязательно
     async def post(self):
         try:
             reader: MultipartReader = await self.request.multipart()
         except AssertionError as exc:
             raise HTTPBadRequest(reason=exc.args[0])
 
+        download_path = os.path.abspath("image_files")
         async for part in reader:
             if part.name == "image":
-                await download_file(part, "image_files", part.filename)
-
-        return json_response()
+                await download_file(part, download_path, part.filename)
+                image_path = os.path.join(download_path, part.filename)
+                image_np = load_image_np(image_path)
+                white_pixels_count= count_pixels(image_np, "#FFFFFF")
+                black_pixels_count = count_pixels(image_np, "#000000")
+        if white_pixels_count > black_pixels_count:
+            major_color = "white"
+        elif white_pixels_count < black_pixels_count:
+            major_color = "black"
+        else:
+            major_color = "equal"
+        return json_response(
+            data={
+                "major_color": major_color,
+            }
+        )
 
 
 class CustomColorView(View):
--- image_app/web/utils.py
+++ image_app/web/utils.py
@@ -6,16 +6,20 @@ from webcolors import hex_to_rgb
 from aiohttp import BodyPartReader
 
 
-def count_pixels(image_path: str, hex_color: str) -> int:
-    image = imread(image_path)
+# TODO: ловить эксепшн при некорректном изображении
+def load_image_np(image_path: str) -> np.ndarray:
+    return imread(image_path)
+
+
+def count_pixels(image: np.ndarray, hex_color: list[str]) -> int:
     rgb_color = hex_to_rgb(hex_color)
     bgr_color = rgb_color[::-1]
     return np.count_nonzero(np.all(image == bgr_color, axis=2))
 
 
-async def download_file(part: BodyPartReader, download_folder: str, filename: str) -> None:
+async def download_file(part: BodyPartReader, download_path: str, filename: str) -> None:
     size = 0
-    with open(os.path.join(os.path.abspath(download_folder), filename), 'wb') as f:
+    with open(os.path.join(download_path, filename), 'wb') as f:
         while True:
             chunk = await part.read_chunk()  # 8192 bytes by default.
             if not chunk:
