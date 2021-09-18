6d14db588b2ebb9dd0935c637ceee76391788a4d
Aleksandr Shcherbakov <sheraleks@outlook.com>
2021-09-18 20:08:43 +0700

    image deserialisation fix

--- image_app/pixels/custom_fields.py
+++ image_app/pixels/custom_fields.py
@@ -1,3 +1,4 @@
+from aiohttp.web_request import FileField
 from marshmallow import fields, ValidationError, pre_load
 
 from image_app.web.utils import load_image
@@ -5,9 +6,10 @@ from image_app.web.utils import load_image
 
 class Image(fields.Field):
     def _deserialize(self, value, attr, data, **kwargs):
-        a = load_image(value.file)
-        return a
+        if not isinstance(value, FileField):
+            raise ValidationError("Must be a file.")
+        return load_image(value.file)
 
     def _validate(self, value):
         if value is None:
-            raise ValidationError("Not a valid image")
+            raise ValidationError("Not a valid image.")
